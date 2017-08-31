b6ef05b9c731ed7a955e7c0f87d4612920367a99
jgaillar <ankadre@hotmail.fr>
2017-08-31 14:04:06 +0200

    oklm

--- src/wolf3d.c
+++ src/wolf3d.c
@@ -44,14 +44,14 @@ void	draw_wolf(t_tmp tmp, t_img *img)
 
 	stuff = tmp.stuff;
 //	printf("%f\n", stuff->wolf.angle);
-	draw.angle = 60;
+	draw.angle = 90;
 	check_hor(stuff->wolf, &draw, stuff->map);
 	// while (tmp.start < tmp.end)
 	// {
-	// 	if (check_hor(stuff->map, &draw) == 1)
+	// 	if (check_hor(stuff->wolf, &draw, stuff->map) == 1)
 	// 	{
 	// 		draw.disthor = draw.dist;
-	// 		check_ver(stuff->map, &draw);
+	// 		check_ver(stuff->wolf, &draw, stuff->map);
 	// 		draw.distver = draw.dist;
 	// 		draw.dist = (draw.disthor < draw.distver ? draw.disthor : draw.distver);
 	// 	//printf("disthor : [%f]\n", draw.dist);
@@ -60,20 +60,55 @@ void	draw_wolf(t_tmp tmp, t_img *img)
 	// 	}
 	// 	draw.angle -= stuff->wolf.angleray;
 	// 	tmp.start++;
+	// }
 }
 
 int	check_hor(t_wolf wolf, t_draw *draw, t_map map)
 {
 	draw->Yi = (draw->angle > 0 && draw->angle <= 180 ? \
-		floor(wolf.posy/64) * 64 - 1 : floor(wolf.posy/64) * 64 + 64);
-	draw->Xi = floor(wolf.posx + (wolf.posy - draw->Yi) / atan(draw->angle));
+		floor(wolf.posy/64) * 64 - 1: floor(wolf.posy/64) * 64 + 64);
+	draw->Xi = floor(wolf.posx + (wolf.posy - draw->Yi) / tan(draw->angle));
 	draw->x = floor(draw->Xi / 64);
 	draw->y = floor(draw->Yi / 64);
 	draw->Ya = (draw->angle > 0 && draw->angle <= 180 ? 64 : -64);
-	draw->Xa = floor((draw->Ya / atan(draw->angle)));
+	draw->Xa = 	(draw->angle > 90 && draw->angle <=360 ? \
+		floor((draw->Ya / fabs(tan(90 - draw->angle)))) : \
+		floor(draw->Ya / tan(draw->angle)));
+	if (draw->angle == 90)
+		draw->Xa = 0;
+	printf("Xi : [%f]\nYi : [%f]\nx : [%i]\ny : [%i]\nXa : [%f]\nYa : [%f]\n\n", draw->Xi, draw->Yi, draw->x, draw->y, draw->Xa, draw->Ya);
 	while (draw->y >= 0 && draw->y < map.maxy && draw->x >= 0 && draw->x < map.maxx)
 	{
-		printf("Yi : [%f]\nXi : [%f]\nx : [%i]\ny : [%i]\nYa : [%f]\nXa : [%f]\n\n", draw->Yi, draw->Xi, draw->x, draw->y, draw->Ya, draw->Xa);
+		if (map.array[draw->y][draw->x] == 1)
+		{
+			ft_putendl("wall");
+			draw->dist = fabs(wolf.posx - draw->Xi) / cos(draw->angle);
+			return (1);
+		}
+		else
+		{
+			draw->Xi += draw->Xa;
+			draw->Yi -= draw->Ya;
+			draw->x = floor(draw->Xi / 64);
+			draw->y = floor(draw->Yi / 64);
+		}
+		printf("Xi : [%f]\nYi : [%f]\nx : [%i]\ny : [%i]\nXa : [%f]\nYa : [%f]\n\n", draw->Xi, draw->Yi, draw->x, draw->y, draw->Xa, draw->Ya);
+	}
+	return (0);
+}
+
+int	check_ver(t_wolf wolf, t_draw *draw, t_map map)
+{
+	draw->Xi = (draw->angle > 90 && draw->angle <= 270 ? \
+		floor(wolf.posx/64) * 64 - 1: floor(wolf.posx/64) * 64 + 64);
+	draw->Yi = floor(wolf.posy + (wolf.posx - draw->Xi) * tan(draw->angle));
+	draw->Xa = (draw->angle > 90 && draw->angle <= 270 ? -64 : 64);
+	draw->Ya = floor(64 * tan(draw->angle));
+	draw->x = floor(draw->Xi / 64);
+	draw->y = floor(draw->Yi / 64);
+	while (draw->y >= 0 && draw->y < map.maxy && draw->x >= 0 && draw->x < map.maxx)
+	{
+		printf("Yi : [%f]\nXi : [%f]\ny : [%i]\nx : [%i]\nYa : [%f]\nXa : [%f]\n\n", draw->Yi, draw->Xi, draw->y, draw->x, draw->Ya, draw->Xa);
 		if (map.array[draw->y][draw->x] == 1)
 		{
 			ft_putendl("wall");
@@ -90,11 +125,6 @@ int	check_hor(t_wolf wolf, t_draw *draw, t_map map)
 	}
 	return (0);
 }
-
-// int	check_ver(t_wolf wolf, t_draw *draw, t_map map)
-// {
-//
-// }
 
 void	correc_dist(t_draw *draw, t_stuff *stuff)
 {
